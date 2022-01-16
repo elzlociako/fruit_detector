bcd4ab5eb29b695f27bfe53a0f4cd1857482566d
Szymon Sobczak <szymonsobczak2282@gmail.com>
2022-01-16 15:15:19 +0100

    Dodanie venv

--- Fruit_Do_It.py
+++ Fruit_Do_It.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import cv2 as cv
  
 def ScaleImage(input_image, scale_val):
@@ -11,7 +10,7 @@ def ScaleImage(input_image, scale_val):
     return result
 
 def main():
-    image = cv.imread('data/03.jpg')
+    image = cv.imread('data/01.jpg')
     
     with open('dnn_model/coco.names', 'r') as file:
         classes = file.read().splitlines()
@@ -42,61 +41,3 @@ def main():
 
 if __name__ == "__main__":
     main()  
-=======
-import cv2 as cv
-import json
-import click
-
-from glob import glob
-from tqdm import tqdm
-
-from typing import Dict
-
-
-def detect_fruits(img_path: str) -> Dict[str, int]:
-    """Fruit detection function, to implement.
-
-    Parameters
-    ----------
-    img_path : str
-        Path to processed image.
-
-    Returns
-    -------
-    Dict[str, int]
-        Dictionary with quantity of each fruit.
-    """
-    img = cv.imread(img_path, cv.IMREAD_COLOR)
-
-    #TODO: Implement detection method.
-    
-    apple = 0
-    banana = 0
-    orange = 0
-
-    return {'apple': apple, 'banana': banana, 'orange': orange}
-
-
-@click.command()
-@click.option('-p', '--data_path', help='Path to data directory')
-@click.option('-o', '--output_file_path', help='Path to output file')
-def main(data_path, output_file_path):
-
-    img_list = glob(f'{data_path}/*.jpg')
-
-    results = {}
-
-    for img_path in tqdm(sorted(img_list)):
-        fruits = detect_fruits(img_path)
-
-        filename = img_path.split('/')[-1]
-
-        results[filename] = fruits
-    
-    with open(output_file_path, 'w') as ofp:
-        json.dump(results, ofp)
-
-
-if __name__ == '__main__':
-    main()
->>>>>>> 91bb03f8f8917e7ffad162bd192277f82c929c50
