815847a6173f4c5e12d34753a7518d649ac64761
Szymon Sobczak <szymonsobczak2282@gmail.com>
2022-01-28 22:29:50 +0100

    final

--- Prototypes/Fruit_Do_It.py
+++ Prototypes/Fruit_Do_It.py
@@ -1,52 +1,69 @@
 import cv2 as cv
-import numpy as np
 
-def ScaleImage(input_img, scale_val):
-    width = int(input_img.shape[1] * scale_val)
-    height = int(input_img.shape[0] * scale_val)
+def ScaleImage(input_image, scale_val):
+    width = int(input_image.shape[1] * scale_val)
+    height = int(input_image.shape[0] * scale_val)
 
     dim = (width, height)
-    output_img = cv.resize(input_img, dim, interpolation = cv.INTER_AREA)
+    result = cv.resize(input_image, dim, interpolation = cv.INTER_AREA)
 
-    return output_img
+    return result
 
 def main():
-    img1 = cv.imread('/home/el_zlociako/Documents/WDPO_PROJ/testing/banana1.png', 1)
-    img2 = cv.imread('/home/el_zlociako/Documents/WDPO_PROJ/data/02.jpg', 1)
+    image = cv.imread("/home/el_zlociako/WDPO_Project/data/05.jpg", cv.IMREAD_COLOR)
 
-    resized1 = ScaleImage(img1, 0.2)
-    resized2 = ScaleImage(img2, 0.2)
-
-    gray1 = cv.cvtColor(resized1 ,cv.COLOR_BGR2GRAY)
-    gray2 = cv.cvtColor(resized2 ,cv.COLOR_BGR2GRAY)
-
-
-    # gray = cv.cvtColor(resized,cv.COLOR_BGR2GRAY)
-
-    # sobel = cv.Sobel(gray,cv.CV_8U,1,1,ksize=5)
-
-    # blur = cv.GaussianBlur(gray, (5,5), 0)
-
-    # edges = cv.Canny(blur,0,200)
-
-    # contours, hierarchy = cv.findContours(edges, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-
-    # chuj = cv.drawContours(resized, contours, -1, (0,255,0), 3)
-    # img2gray = cv.cvtColor(img_resized,cv.COLOR_BGR2GRAY)
-    # img2gray = np.float32(img2gray)
-    # img_blur = cv.medianBlur(img2gray, 3)
-
-    # ret, mask = cv.threshold(img2gray, 120, 150, cv.THRESH_BINARY)
-    # mask_inv = cv.bitwise_not(mask)
+    orange_counter = 0
+    banana_counter = 0
+    apple_counter = 0
     
-    # dst = cv.cornerHarris(mask_inv,2,3,0.04)
-    # img_resized[dst>0.01*dst.max()]=[255,0,0]
+    with open('dnn_model/coco.names', 'r') as file:
+        classes = file.read().splitlines()
+    
+    net = cv.dnn.readNetFromDarknet('dnn_model/yolov4.cfg', 'dnn_model/yolov4.weights')
+    
+    model = cv.dnn_DetectionModel(net)
+    model.setInputParams(scale=1 / 255, size=(416, 416), swapRB=True)
+    
+    classIds, scores, boxes = model.detect(image, confThreshold=0.5, nmsThreshold=0.5)
+    
+    for (classId, score, box) in zip(classIds.squeeze(), scores, boxes):
+        fruit_name = classes[classId]
 
-    # cv.imshow('1', sobel)
-    # cv.imshow('2', edges)
-    cv.imshow('1', imgKp1)
-    cv.imshow('2', imgKp2)
-    cv.imshow('3', img3)
+        if fruit_name == "orange":
+            cv.rectangle(image, (box[0], box[1]), (box[0] + box[2], box[1] + box[3]),
+                    color=(0,165,255), thickness=5)
+            cv.putText(image, fruit_name, (box[0], box[1]+box[3]-20), cv.FONT_HERSHEY_SIMPLEX, 2,
+                    color=(0, 0, 0), thickness=5)
+            orange_counter = orange_counter + 1
+        
+        if fruit_name == "apple":
+            cv.rectangle(image, (box[0], box[1]), (box[0] + box[2], box[1] + box[3]),
+                    color=(0,0,255), thickness=5)
+            cv.putText(image, fruit_name, (box[0], box[1]+box[3]-20), cv.FONT_HERSHEY_SIMPLEX, 2,
+                    color=(0, 0, 0), thickness=5)
+            apple_counter = apple_counter + 1
+
+        if fruit_name == "banana":
+            cv.rectangle(image, (box[0], box[1]), (box[0] + box[2], box[1] + box[3]),
+                    color=(0,255,255), thickness=5)
+            cv.putText(image, fruit_name, (box[0], box[1]+box[3]-20), cv.FONT_HERSHEY_SIMPLEX, 2,
+                    color=(0, 0, 0), thickness=5)
+            banana_counter = banana_counter + 1
+
+    out_image = ScaleImage(image, 0.3)
+    
+    banana_str = str(banana_counter)
+    apple_str = str(apple_counter)
+    orange_str = str(orange_counter)
+    
+    cv.putText(out_image, ("Bananas: " + banana_str), (out_image.shape[1]-100, 20), cv.FONT_HERSHEY_SIMPLEX, 0.5,
+                    color=(0, 0, 0), thickness=1)
+    cv.putText(out_image, ("Apples: " + apple_str ), (out_image.shape[1]-100, 40), cv.FONT_HERSHEY_SIMPLEX, 0.5,
+                    color=(0, 0, 0), thickness=1)
+    cv.putText(out_image, ("Oranges: " + orange_str ), (out_image.shape[1]-100, 60), cv.FONT_HERSHEY_SIMPLEX, 0.5,
+                    color=(0, 0, 0), thickness=1)
+
+    cv.imshow('Fruits Recognison', out_image)
 
     while True:
         if cv.waitKey(1) == ord('q'):
@@ -54,7 +71,5 @@ def main():
 
     cv.destroyAllWindows()
 
-
 if __name__ == "__main__":
     main()  
-
